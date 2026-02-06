1d6ad7413ad6f462816007fdd54b7f1678fad278
andrei <protoandrei@protonmail.com>
2026-02-06 15:50:30 +0100

    Added `invoice_type` and `school_invoice_date` fields in the school model, updated invoice creation logic to support student-specific billing, and adjusted related views for improved usability.

--- ps_school/models/schools.py
+++ ps_school/models/schools.py
@@ -10,8 +10,6 @@ _logger = logging.getLogger(__name__)
 class School(models.Model):
     _inherit = "res.partner"
 
-    is_school = fields.Boolean(string='Es colegio')
-
     course_line_ids = fields.One2many(
         comodel_name='school.course.line',
         inverse_name='school_id',
@@ -20,6 +18,9 @@ class School(models.Model):
     student_qty = fields.Integer(string='Alumnos totales', compute='_compute_student_qty', store=True)
     active_student_qty = fields.Integer(string='Alumnos de alta', compute='_compute_student_qty', store=True)
     inactive_student_qty = fields.Integer(string='Alumnos de baja', compute='_compute_student_qty', store=True)
+    invoice_type = fields.Selection([('0', 'A la escuela'), ('1', 'A los alumnos')], string='Tipo de facturación',
+                                    default='0')
+    school_invoice_date = fields.Date(string='Ultima facturación de la escuela', readonly=True)
 
     @api.depends('course_line_ids.student_ids', 'course_line_ids.student_ids.enrollment_state')
     def _compute_student_qty(self):
@@ -34,23 +35,66 @@ class School(models.Model):
         for record in self:
             # Get the product
             product = self.env.ref('ps_school.active_student_product')
-            invoice = record.env['account.move'].create({
-                'move_type': 'out_invoice',
-                'partner_id': record.id,
-                'invoice_date': fields.Date.today(),
-                'invoice_line_ids': [(0, 0, {
-                    'product_id': product.id,
-                    'quantity': record.active_student_qty,
-                    'tax_ids': [(6, 0, product.taxes_id.ids)],
-                    'price_unit': product.list_price
-                })],
-            })
 
-            return {
-                'type': 'ir.actions.act_window',
-                'name': 'Factura',
-                'res_model': 'account.move',
-                'view_mode': 'form',
-                'res_id': invoice.id,
-                'target': 'current',
-            }
+            if record.active_student_qty == 0 or not record.course_line_ids:
+                raise UserError('No hay alumnos para facturar')
+
+            if record.invoice_type == '0':
+                invoice = record.env['account.move'].create({
+                    'move_type': 'out_invoice',
+                    'partner_id': record.id,
+                    'invoice_date': fields.Date.today(),
+                    'invoice_line_ids': [(0, 0, {
+                        'product_id': product.id,
+                        'quantity': record.active_student_qty,
+                        'tax_ids': [(6, 0, product.taxes_id.ids)],
+                        'price_unit': product.list_price
+                    })],
+                })
+
+                record.school_invoice_date = fields.Date.today()
+
+                return {
+                    'type': 'ir.actions.act_window',
+                    'name': 'Factura',
+                    'res_model': 'account.move',
+                    'view_mode': 'form',
+                    'res_id': invoice.id,
+                    'target': 'current',
+                }
+            elif record.invoice_type == '1':
+                students = record.course_line_ids.mapped('student_ids')
+
+                invoices = []
+                for student in students:
+                    invoice_parent = student.guardian_ids.filtered(lambda g: g.invoice)[0].partner_id.id,
+
+                    if not invoice_parent:
+                        raise UserError(f'El alumno {student.name} no tiene un padre al que facturar. {student.id}')
+                    elif student.enrollment_state == 'active':
+                        invoices.append({
+                            'move_type': 'out_invoice',
+                            'partner_id': invoice_parent,
+                            'invoice_date': fields.Date.today(),
+                            'invoice_line_ids': [(0, 0, {
+                                'product_id': product.id,
+                                'name': f"{product.name} - {student.name}",
+                                'quantity': 1,
+                                'tax_ids': [(6, 0, product.taxes_id.ids)],
+                                'price_unit': product.list_price
+                            })],
+                        })
+
+                record.env['account.move'].create(invoices)
+
+                record.school_invoice_date = fields.Date.today()
+
+                return {
+                    'type': 'ir.actions.act_window',
+                    'name': 'Facturas',
+                    'res_model': 'account.move',
+                    'view_mode': 'list,form',
+                    'views': [(self.env.ref('account.view_out_invoice_tree').id, 'list')],
+                    'domain': [('move_type', '=', 'out_invoice')],
+                    'target': 'current',
+                }
